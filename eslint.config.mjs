d7ad27dda67dfd387f6e36e781d1ccf7476b8619
AdnanAyaz1 <adnanayaz479@gmail.com>
2025-12-25 02:03:12 -0800

    setup eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,6 +13,44 @@ const eslintConfig = defineConfig([
     "build/**",
     "next-env.d.ts",
   ]),
+  {
+    ignores: ["components/ui/**/*"],
+  },
+  {
+    rules: {
+      "import/order": [
+        "error",
+        {
+          groups: ["builtin", "external", "internal", ["parent", "sibling"], "index", "object"],
+
+          "newlines-between": "always",
+
+          pathGroups: [
+            {
+              pattern: "@app/**",
+              group: "external",
+              position: "after",
+            },
+          ],
+
+          pathGroupsExcludedImportTypes: ["builtin"],
+
+          alphabetize: {
+            order: "asc",
+            caseInsensitive: true,
+          },
+        },
+      ],
+      "comma-dangle": "off",
+    },
+  },
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+
+    rules: {
+      "no-undef": "off",
+    },
+  },
 ]);
 
 export default eslintConfig;
